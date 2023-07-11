8f42af0af330c9a207bcdc0fdf5c6c904ce73305
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-07-11 16:40:54 -0400

    My lab solution

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-
+    
+    def name=(dog_name) # setter method takes in an argument (dog_name)
+    @this_dogs_name = dog_name  # sets the @this_dogs_name instance variable equal to the value of the argument
+    end
+    
+    def name # getter method is responsible for getting/returning the value of the instance variable
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new  # Creating a new Dog instance
+lassie.name = "lassie" # Assigning a name to the dog instance
+
+puts lassie.name # Call the Dog#name method, asking it to return its name
