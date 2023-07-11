8b43a4570e97032869d0027b4b55961aa1d547a3
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-07-11 16:07:23 -0400

    commit tree 41bb685a021e4fefaf39eff9ff20078feaec29b7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
 
+    def name=(dog_name) #this method is responsible for setting the name 
+    @this_dogs_name = dog_name
+    end
+
+    def name # this method is responsible for getting an individual dog's name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new  # Creating a new Dog instance
+lassie.name = "lassie" # Assigning a name to the dog instance
+
+puts lassie.name # Asking it to return its name
